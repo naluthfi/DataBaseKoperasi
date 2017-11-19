08e9767e3f3d3f5dcc7f8f6d5fe5621d4ae4aedc
Unknown <naluthfi@gmail.com>
2017-11-19 19:29:53 +0700

    Form Login

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connstring = "Server=127.0.0.1; Port=5432; User Id=postgres; Password=hahaha; Database=Koperasi;";
+            NpgsqlConnection connection = new NpgsqlConnection(connstring);
+            connection.Open();
+            string namalogin = TBUsername.Text;
+            string password = TBPassword.Text;
+            NpgsqlCommand cmd = new NpgsqlCommand("select nm_login,pass_login from pengguna where nm_login='" + TBUsername.Text + "'and pass_login='" + TBPassword.Text + "'", connection);
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Login berhasil.");
+            }
+            else
+            {
+                MessageBox.Show("Login Gagal. Silahkan cek kembali nama login dan password anda.");
+            }
+            connection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ WindowsFormsApp1/MainForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUser = new System.Windows.Forms.Button();
+            this.buttonAdministrasi = new System.Windows.Forms.Button();
+            this.buttonTPUmum = new System.Windows.Forms.Button();
+            this.buttonTPAnggota = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonUser
+            // 
+            this.buttonUser.Location = new System.Drawing.Point(13, 13);
+            this.buttonUser.Name = "buttonUser";
+            this.buttonUser.Size = new System.Drawing.Size(75, 23);
+            this.buttonUser.TabIndex = 0;
+            this.buttonUser.Text = "User";
+            this.buttonUser.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdministrasi
+            // 
+            this.buttonAdministrasi.Location = new System.Drawing.Point(106, 13);
+            this.buttonAdministrasi.Name = "buttonAdministrasi";
+            this.buttonAdministrasi.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdministrasi.TabIndex = 1;
+            this.buttonAdministrasi.Text = "Administrasi";
+            this.buttonAdministrasi.UseVisualStyleBackColor = true;
+            // 
+            // buttonTPUmum
+            // 
+            this.buttonTPUmum.Location = new System.Drawing.Point(13, 52);
+            this.buttonTPUmum.Name = "buttonTPUmum";
+            this.buttonTPUmum.Size = new System.Drawing.Size(168, 23);
+            this.buttonTPUmum.TabIndex = 2;
+            this.buttonTPUmum.Text = "Transaksi Penjualan Umum";
+            this.buttonTPUmum.UseVisualStyleBackColor = true;
+            // 
+            // buttonTPAnggota
+            // 
+            this.buttonTPAnggota.Location = new System.Drawing.Point(13, 91);
+            this.buttonTPAnggota.Name = "buttonTPAnggota";
+            this.buttonTPAnggota.Size = new System.Drawing.Size(168, 23);
+            this.buttonTPAnggota.TabIndex = 3;
+            this.buttonTPAnggota.Text = "Transaksi Penjualan Anggota";
+            this.buttonTPAnggota.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(194, 127);
+            this.Controls.Add(this.buttonTPAnggota);
+            this.Controls.Add(this.buttonTPUmum);
+            this.Controls.Add(this.buttonAdministrasi);
+            this.Controls.Add(this.buttonUser);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUser;
+        private System.Windows.Forms.Button buttonAdministrasi;
+        private System.Windows.Forms.Button buttonTPUmum;
+        private System.Windows.Forms.Button buttonTPAnggota;
+    }
+}
